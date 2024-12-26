2e665284c8044b749e69bb3e179cb7518112d2e6
Soujujuliana <julianaelisasousa87@gmail.com>
2024-12-26 13:26:05 -0300

    Desafio definitivo

--- Index.js
+++ Index.js
@@ -1,1 +1,32 @@
-console.log ("meu primeiro projeto");
+let xp = 7800;
+
+	if (xp < 1000)
+        {console.log ("O Herói de nome Juliana está no nível Ferro");}
+     
+    if (xp >= 1001 && xp <= 2000)
+    	{console.log ("O Herói de nome Juliana está no nível Bronze");}
+
+    if(xp >= 2001 && xp <= 5000)
+        {console.log ("O Herói de nome Juliana está no nível Prata");}
+
+    if(xp >= 5001 && xp <= 7000)
+        {console.log ("O Herói de nome Juliana está no nível Ouro");}
+
+    if(xp >= 7001 && xp <= 8000)
+        {console.log ("O Herói de nome Juliana está no nível Platina");}
+
+    if(xp >= 8001 && xp <= 9000)
+        {console.log ("O Herói de nome Juliana está no nível Ascendente");}
+
+    if(xp >= 9001 && xp <= 10000)
+        {console.log ("O Herói de nome Juliana está no nível Imortal");}
+
+    if(xp >= 10001)
+        {console.log ("O Herói de nome Juliana está no nível Radiante");}
+
+
+    
+
+
+       
+// O VS NÂO aceitou o else if de jeito nenhum! Quando tentei colocar dsava erro de sintaxe!
