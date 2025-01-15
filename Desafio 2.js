2f37b9f68dbc1fca71810ef4dab33a2d8435e93e
Soujujuliana <107082479+Soujujuliana@users.noreply.github.com>
2025-01-15 13:10:02 -0300

    Add files via upload

---
+++ Desafio 2.js
@@ -0,0 +1,33 @@
+
+function rankeadas( vitorias, derrotas){
+
+	let resultado = vitorias-derrotas
+    return(resultado)
+}
+    let posicao = rankeadas(5, 5)
+	    console.log("O herói teve o resultado de " + posicao + " vitórias e está no nível de Ferro!")
+
+    let posicao2 = rankeadas(25, 11)
+	    console.log("O herói teve o resultado de " + posicao2 + " vitórias e está no nível de Bronze!")
+
+    let posicao3 = rankeadas(50, 25)
+	    console.log("O herói teve o resultado de " + posicao3 + " vitórias e está no nível de Prata!")
+
+    let posicao4 = rankeadas(180, 100)
+	    console.log("O herói teve o resultado de " + posicao4 + " vitórias e está no nível de Ouro!")
+
+    let posicao5 = rankeadas(190, 100)
+	    console.log("O herói teve o resultado de " + posicao5 + " vitórias e está no nível de Diamante!")
+
+    let posicao6 = rankeadas(200, 100)
+	    console.log("O herói teve o resultado de " + posicao6 + " vitórias e está no nível de Lendário!")
+
+    let posicao7 = rankeadas(202, 101)
+	    console.log("O herói teve o resultado de " + posicao7 + " vitórias e está no nível de Imortal!")
+
+
+
+
+
+
+
